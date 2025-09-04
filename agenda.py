c766da7f02316024b80c7a36fc89f927ef91f817
Diego <kharontees@gmail.com>
2025-09-04 14:24:44 -0400

    agenda 0.1.1

--- agenda.py
+++ agenda.py
@@ -1,22 +1,35 @@
 #agenda
 contactos = []
 def agregar_contactos(nombre, fono):
-    contactos.append({"nombre":nombre, "telefono":fono})
+    contactos.append({"nombre":nombre, "fono":fono})
 
 def listar_contactos():
     print("\nLista de contactos")
     for c in contactos:
-        print(f"Nombre: {c['nombre']}, Teléfono: {c['telefono']}")
+        print(f"Nombre: {c['nombre']}, Teléfono: {c['fono']}")
 
+def buscar_contacto(nombre):
+    for c in contactos:
+        if c['nombre'].lower() == nombre.lower():
+            print("\n")
+            print(f"Contacto encontrado: {c['nombre']} - {c['fono']}")
+            return True
+    print("Contacto no encontrado")
 
 #Ingreso de datos
-nombre = input("Ingresa nombre: ")
-telefono = input("Ingresa num de telefono: ")
+while True:
+    nombre = input("Ingresa nombre: ")
+    fono = input("Ingresa num de telefono: ")
 
-#Agrega a la lista y despuesa gregar
-agregar_contactos(nombre,telefono)
-print("Contacto agregado")
+    #Agrega a la lista y despuesa gregar
+    agregar_contactos(nombre,fono)
+    print("Contacto agregado")
+    
+    #Lista los contactos
+    listar_contactos()
 
-#Lista los contactos
-listar_contactos()
+    nombre_buscar = input("Nombre a buscar: ")
+
+    if buscar_contacto(nombre_buscar) == True:
+        break
 
